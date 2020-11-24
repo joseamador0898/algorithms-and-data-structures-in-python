5fb678c10abc34964edcc24e432453866ca3ef5f
Jose Amador <jose.amador0898@gmail.com>
2020-11-24 17:58:05 -0600

    Added climbing stairs solution

--- dynamic_promgramming/climbing_stairs.py
+++ dynamic_promgramming/climbing_stairs.py
@@ -1,4 +1,21 @@
-#https://leetcode.com/problems/climbing-stairs/
-#Climbing Stairs
-#Dynamic Programming
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 2:
+            return n
+        #Initialize dp array
+        dp = [0] * n
 
+        #Base case n=1, only one way to get to step 1
+        #0 -> 1
+        dp[0] = 1
+
+        #Base case n=2, two ways to get to step 2
+        #0 -> 1 -> 2
+        #0 -> 2
+        dp[1] = 2
+
+        #Bottom-up DP approach to calculate no. of ways for N steps
+        for i in range(2, n):
+            dp[i] = dp[i-2] + dp[i-1]
+        
+        return dp[n-1]
